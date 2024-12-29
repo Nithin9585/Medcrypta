a3f261052aef13801567c383a8024d1311edbd4c
Nithin9585 <nithin958595@gmail.com>
2024-12-29 20:35:15 +0530

    ui changes

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
+import Footer from "@/components/ui/footer";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +25,15 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem>
+          
+        <Navbar />
         {children}
+        <Footer /> 
+        </ThemeProvider> 
       </body>
     </html>
   );
