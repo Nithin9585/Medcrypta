dae018cd42b9d988863a5693b75c2f83fe8d87e4
shreyanshVIT23 <shreyansh.23bce10701@vitbhopal.ac.in>
2025-02-04 23:32:40 +0530

    added jwt authentication to flask

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from .utils .mongo_wrapper import MongoDBManager
+from flask import Flask, json
+from flask_jwt_extended import JWTManager
+from .utils.mongo_wrapper import MongoDBManager
 from .loader import ConfigLoader
 
 
@@ -8,20 +9,29 @@ def create_app():
     app = Flask(__name__)
 
     try:
-        app .config["MONGO_URI"] = ConfigLoader .get_env_variable("MONGO_URI")
-        app .config["SECRET_KEY"] = ConfigLoader .get_env_variable(
-            "SECRET_KEY")
+        app.config["MONGO_URI"] = ConfigLoader.get_env_variable("MONGO_URI")
+        app.config["SECRET_KEY"] = ConfigLoader.get_env_variable("SECRET_KEY")
+        app.config["MONGO_DB_NAME"] = ConfigLoader.get_env_variable("MONGO_DB_NAME")
+        app.config["JWT_SECRET_KEY"] = ConfigLoader.get_env_variable("JWT_SECRET_KEY")
+        app.config["JWT_TOKEN_LOCATION"] = json.loads(
+            ConfigLoader.get_env_variable("JWT_TOKEN_LOCATION")
+        )
     except KeyError as e:
         raise RuntimeError(f"Missing required environment variable: {e}")
 
-    MongoDBManager .init_app(app)
+    MongoDBManager.init_app(app)
+    jwt = JWTManager(app)
 
-    from app .routes .mongo_api import main_bp
-    app .register_blueprint(main_bp)
+    from app.routes.mongo_api import main_bp
 
-    @app .cli .command("shutdown")
+    app.register_blueprint(main_bp)
+    from app.routes.authorized_api import admin_bp
+
+    app.register_blueprint(admin_bp)
+
+    @app.cli.command("shutdown")
     def shutdown():
-        MongoDBManager .close_connection()
+        MongoDBManager.close_connection()
 
     return app
 
@@ -30,6 +40,8 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app .logger .info(f"""Starting Flask app with MongoDB URI: {
-        app .config['MONGO_URI']}""")
-    app .run(debug=True)
+    app.logger.info(
+        f"""Starting Flask app with MongoDB URI: {
+        app .config['MONGO_URI']}"""
+    )
+    app.run(debug=True)
