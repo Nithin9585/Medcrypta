f81415add823e493caa97f9f9f3c7983aaa5282e
shreyanshVIT23 <shreyansh.23bce10701@vitbhopal.ac.in>
2025-02-04 23:36:59 +0530

    fixed generate_key error

--- backend/app/utils/encryption_utils.py
+++ backend/app/utils/encryption_utils.py
@@ -1,15 +1,15 @@
-from cryptography .fernet import Fernet
+from cryptography.fernet import Fernet
+from ..loader import ConfigLoader
 
-
-SECRET_KEY = Fernet .generate_key()
+SECRET_KEY = ConfigLoader.get_env_variable("SECRET_KEY")
 cipher = Fernet(SECRET_KEY)
 
 
 def encrypt_data(data: str) -> str:
 
-    return cipher .encrypt(data .encode()).decode()
+    return cipher.encrypt(data.encode()).decode()
 
 
 def decrypt_data(encrypted_data: str) -> str:
 
-    return cipher .decrypt(encrypted_data .encode()).decode()
+    return cipher.decrypt(encrypted_data.encode()).decode()
