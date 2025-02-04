0681c3a9b19af9eca5a815fa6a86dd9489ec3592
Nithin9585 <nithin958595@gmail.com>
2025-02-04 18:46:37 +0530

    corrected card hover

--- frontend/components/config/Homecomponent.config.js
+++ frontend/components/config/Homecomponent.config.js
@@ -1,5 +1,5 @@
 export const buttonData = [
-  { name: 'Doctor', link: '/Doctor/dashboard',description:'Register as a Doctor',imgsrc:'https://res.cloudinary.com/dvmmuj2r0/image/upload/v1738639951/medcrypta/f7u3kr7igc1yw5yqdy9l.jpg'},
-  { name: 'Patient', link: '/Patient/dashboard',description:'Register as a patiennt',imgsrc:'https://res.cloudinary.com/dvmmuj2r0/image/upload/v1738639948/medcrypta/khcqinzzvcir7gwekudk.jpg'},
+  { name: 'Doctor', link: '/Doctor/dashboard',description:'Register as a Doctor',imgsrc:'https://res.cloudinary.com/dvmmuj2r0/image/upload/v1738674896/medcrypta/onfxiocvngsvvwue0wnl.png'},
+  { name: 'Patient', link: '/Patient/dashboard',description:'Register as a patiennt',imgsrc:'https://res.cloudinary.com/dvmmuj2r0/image/upload/v1738674892/medcrypta/kqofusbn9w2ip8n99css.png'},
   { name: 'Pharmasist', link: '/Pharmasist/dashboard',description:'Register as a pharmasist',imgsrc:'https://res.cloudinary.com/dvmmuj2r0/image/upload/v1738673634/medcrypta/mdnwsbhcfahwzgpzn2s8.png'}
 ];
